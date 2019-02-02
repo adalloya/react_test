1837d1f4a8cf009a77bf47b99fa52bc94c9a498c
Adal Loya <adalloya@gmail.com>
2019-02-01 19:34:34 -0600

    hello

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -25,9 +25,9 @@ function Nav() {
                     <li><a href="#modal4" className="modal-trigger"><i id="viewicon"className="material-icons ">view_module</i>Mis anuncios</a></li>
                     <li> <a id="usericon" className="modal-trigger" href="#modal2"><i className="material-icons">account_circle</i>Mi Cuenta</a></li>
                     <li> <a id="posticon" className="btn-floating btn-large waves-effect waves-light red btn modal-trigger pulse hide" href="#modal1"><i className="material-icons">add</i>Publicar anuncio</a></li>
-                    <li><a href="hola" id="number" className="waves-effect waves-light btn-large banner">0 Articulos en venta</a></li>
-                    <li><a href="hola" id="number1" className="waves-effect waves-light btn-large watchers banner">0 Personas comprando</a></li>
-                    <li><a href="hola" id="number2" className="waves-effect waves-light btn-large watchers banner">0 Usuarios registrados</a></li>
+                    <li><a href="hola" id="numbermobile" className="waves-effect waves-light btn-large banner">0 Articulos en venta</a></li>
+                    <li><a href="hola" id="numbermobile1" className="waves-effect waves-light btn-large watchers banner">0 Personas comprando</a></li>
+                    <li><a href="hola" id="numbermobile2" className="waves-effect waves-light btn-large watchers banner">0 Usuarios registrados</a></li>
   </ul>
   </div>
  
